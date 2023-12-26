4938d782389c7643610db8f55598a8952031d738
KinnearTechnologies22 <kinneartechnologies22@gmail.com>
2023-12-26 15:14:28 +0200

    updates

--- src/main.js
+++ src/main.js
@@ -5,10 +5,7 @@ import store from './store'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
-<<<<<<< HEAD
 import 'bootstrap/js/src/util/backdrop.js' 
-=======
->>>>>>> 49cd70f91ed644841ab1717e23f7fa3125ec8be9
 import 'bootstrap'
 import 'aos/dist/aos.css'
 
