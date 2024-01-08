5cca9ad22b7b861af67633720c3e4cf90c6f2ec3
KinnearTechnologies22 <kinneartechnologies22@gmail.com>
2024-01-08 13:07:39 +0200

    updates

--- src/router/index.js
+++ src/router/index.js
@@ -4,21 +4,21 @@ import PageUnavailable from '../views/PageUnavailableView.vue'
 import ConstructionPage from '../views/ConstructionView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: MainView //Change this back to MainView.vue once maintenance is complete
-  },
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: MainView //Change this back to MainView.vue once maintenance is complete
+  // },
   {
     path: '/page-unavailable',
     name: 'pageUnavailable',
     component: PageUnavailable
   },
-  // {
-  //   path: '/',
-  //   name: 'maintenance-page',
-  //   component: ConstructionPage
-  // }
+  {
+    path: '/',
+    name: 'maintenance-page',
+    component: ConstructionPage
+  }
 ]
 
 const router = createRouter({
